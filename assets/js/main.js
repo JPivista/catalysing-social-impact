77b6eff08a2288833b40dc7d844b7c5d99dabcfd
kishorebarath011099@gmail.com <kishorebarath011099@gmail.com>
2024-06-20 15:49:59 +0530

    desktop and mobile header overview is updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -345,25 +345,3 @@ $(".banner-slider").slick({
 })
 
 // Header
-
-function toggleDropdown() {
-  var dropdownContent = document.getElementById("myCustomDropdown")
-  if (dropdownContent.style.display === "block") {
-    dropdownContent.style.display = "none"
-  } else {
-    dropdownContent.style.display = "block"
-  }
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches(".custom-dropbtn")) {
-    var dropdowns = document.getElementsByClassName("custom-dropdown-content")
-    for (var i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i]
-      if (openDropdown.style.display === "block") {
-        openDropdown.style.display = "none"
-      }
-    }
-  }
-}

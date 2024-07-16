2cb6cbeb5430ff70deaed1d3570ce9c70243d75e
Bhavankumarg <gbhavanbkr@gmail.com>
2024-07-16 18:26:10 +0530

    agenda page popup

---
+++ assets/js/agenda.js
@@ -0,0 +1,319 @@
+// Function to open Popup 1
+function openPopup1() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+        <h4 class="py-3 fw-bold">Demystifying Outcomes-Based Finance (OBF): A Practical Guide and AMA Session</h4>
+        <p>Outcomes-based financing (OBF) is an increasingly popular approach to development finance, but there are deep-rooted questions and concerns about what OBF looks like in practice. This session, led by the British Asian Trust (BAT), will explore how OBF can be unlocked and more easily accessed, aiming to mitigate risks and enable sustainable investments.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <img class="popup-img w-25" src="assets/images/logo/british.png" alt="Image 1">
+        
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+// Function to open Popup 2
+function openPopup2() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold">Private Financing for Decarbonization: Community-Based Ecosystem Services for Commons</h4>
+        <p>This session explores innovative approaches to financing value chains through a Payment for Ecosystem Services model. Emphasising small and marginal farmers, particularly women, it focuses on promoting ecological restoration and maintaining ecosystem balance.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div class="d-flex gap-5">
+        <img class="popup-img w-25" src="assets/images/logo/common.png" alt="Image 1">
+        <img class="popup-img w-25" src="assets/images/logo/vrutti.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+// Function to open Popup 3
+function openPopup3() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+       <h4 class="py-3 fw-bold"> Demystifying Blended Finance: Philanthropic Capital Solutions for Commercial Social Ventures</h4>
+        <p>This session brings together donors, structuring wizards, and legal experts to showcase the latest funding trends for social enterprises. Through case studies, it will highlight how philanthropic capital can catalyse the private sector to drive sustainability and scale.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div class="d-flex gap-5">
+        <img class="popup-img w-25" src="assets/images/logo/blended.png" alt="Image 1">
+        <img class="popup-img w-25" src="assets/images/logo/waterfield.png" alt="Image 1">
+        <img class="popup-img w-25" src="assets/images/logo/gamer.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+// Function to open Popup 4
+function openPopup4() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold">Accelerating Implementation and Shaping Policy for the Social Stock Exchange</h4>
+        <p>Co-hosted by the Institute of Cost Accountants of India-Social Auditors Organization (ICMAI-SAO), this session offers a platform for engagement with leading experts to explore pathways for sustainable development via the Social Stock Exchange (SSE).
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div class="d-flex gap-5">
+        <img class="popup-img w-25" src="assets/images/logo/icmai.png" alt="Image 1">
+        <img class="popup-img w-25" src="assets/images/logo/catalyst.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+// Function to open Popup 5
+function openPopup5() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold">AI and No Code Tools for Impact</h4>
+        <p>In today’s digital landscape, integrating technology into social impact projects is essential. This session equips stakeholders with the knowledge to harness AI and No-code tools for impactful outcomes.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div>
+        <img class="popup-img w-25" src="assets/images/logo/future.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+// Function to open Popup 6
+function openPopup6() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold"> Bridging the Gap: Collaborative Strategies for Development Sector Professionals and Government</h4>
+        <p>This masterclass focuses on collaborative strategies to bridge the gap between sector professionals and government for enhanced impact.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div>
+        <img class="popup-img w-25" src="assets/images/logo/nexus.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+// Function to open Popup 7
+function openPopup7() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold">Reimagining Implementation@Scale</h4>
+        <p>This session will enumerate challenges faced by states, identifying roles that CSOs, funders, and states can play in creating an enabling environment for scalable programs.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div>
+        <img class="popup-img w-25" src="assets/images/logo/socion.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+// Function to open Popup 8
+function openPopup8() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold">Accelerate Sustained and Effective Market Access for MSMEs</h4>
+        <p>This workshop will explore innovative solutions for enhancing MSME market access, focusing on sustainable growth and resilience.</p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div class="d-flex gap-5">
+        <img class="popup-img w-25" src="assets/images/logo/cms.png" alt="Image 1">
+        <img class="popup-img w-25" src="assets/images/logo/vrutti.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+// Function to open Popup 9
+function openPopup9() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold">Social Protection Coalition Launch</h4>
+        <p>A round table and launch session to discuss and strategize social protection for vulnerable communities, promoting resilience and inclusion.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div>
+        <img class="popup-img w-25" src="assets/images/logo/cms.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+// Function to open Popup 10
+function openPopup10() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold">Lessons from Scaling with Government</h4>
+        <p>This World Cafe session will explore collaborations with government to achieve scale, sharing lessons from successful case studies.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div>
+        <img class="popup-img w-25" src="assets/images/logo/nexus.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+// Function to open Popup 11
+function openPopup11() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold">Global Warming: A Systems Mapping Game</h4>
+        <p>This interactive group game will delve into the complexities of global warming, providing participants with a hands-on learning experience.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div>
+        <img class="popup-img w-25" src="assets/images/logo/si.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+// Function to open Popup 12
+function openPopup12() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold">Building Resilience for Vulnerable Populations: Systemic Risk and Response Perspective</h4>
+        <p>A World Cafe session to explore strategies to assess and respond to systemic risks, enhancing resilience in the face of global challenges.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div>
+        <img class="popup-img w-25" src="assets/images/logo/asra.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+// Function to open Popup 13
+function openPopup13() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold">Youth-Led Food Transformation Using Agroecology Modules</h4>
+        <p>This workshop will launch a guidebook for adolescents and youth, focusing on sustainable food practices and revitalising local ecosystems.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div>
+        <img class="popup-img w-25" src="assets/images/logo/farm.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+// Function to open Popup 14
+function openPopup14() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold">Frightening Futures, Nourishing Now</h4>
+        <p>An interactive workshop gathering climate leaders and changemakers to discuss strategies for a sustainable and hopeful future.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div>
+        <img class="popup-img w-25" src="assets/images/logo/community.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+// Function to open Popup 15
+function openPopup15() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold">Procure with Purpose: Building a Coalition for Social Procurement in India</h4>
+        <p>This closed-door workshop will focus on building a coalition for social procurement in India, aligning with SDGs and promoting economic and social development.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div>
+        <img class="popup-img w-25" src="assets/images/logo/sap.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+// Function to open Popup 16
+function openPopup16() {
+    var popup = document.getElementById("popup");
+    var contentArea = document.getElementById("popup-content-area");
+
+    var contentHTML = `
+         <h4 class="py-3 fw-bold">MISFITS Pitch</h4>
+        <p>The fourth edition of Misfits Global provides a platform for social entrepreneurs and innovators across Asia to showcase solutions and connect with key stakeholders. This pitch competition will highlight impactful ideas and foster collaboration in the social impact ecosystem.
+        </p>
+        <h5 class="py-3 fw-bold">Collaborator:</h5>
+        <div>
+        <img class="popup-img w-25" src="assets/images/logo/sap.png" alt="Image 1">
+        </div>
+    `;
+
+    contentArea.innerHTML = contentHTML;
+    popup.style.display = "block";
+}
+
+
+// Function to close the popup
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
+
+// Close the popup if user clicks outside of it
+window.onclick = function (event) {
+    var popup = document.getElementById("popup");
+    if (event.target == popup) {                           
+        popup.style.display = "none";
+    }
+};
